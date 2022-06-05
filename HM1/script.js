14bdaed3f87ae993d99ee4920be15cba83a3caab
inferma1 <gusev28@ukr.net>
2022-06-05 23:13:21 +0300

    JS-HW1

---
+++ HM1/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const varName = "Vova";
+typeof varName === 'string' ? 
+console.log('Привет ' + varName) : 
+console.log('Ошибка, не тот тип данных');
+
+
+console.log (typeof 23); 
+console.log(typeof NaN);
+console.log (typeof "Hi my name Vova"); 
+console.log (typeof 10>1);
+console.log (typeof true);
+console.log (typeof {name:"Vova"}); 
+console.log (typeof null );
+console.log (typeof MyNameVova);
+console.log (typeof 234324342n);
+console.log (typeof Symbol( "Age" ));
+
+
+
+
+
+
+
